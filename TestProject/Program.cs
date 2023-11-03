0b5bc4d6e8b1659474bf73ed4768a0194c6b9365
Paulo Cremas <cremascopaulo@gmail.com>
2023-11-03 03:00:38 -0300

    commit final

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,10 @@
 ﻿string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox") == false);
 Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox") == false);
+// Console.WriteLine(!pangram.Contains("fox"));
 
 // string pangram = "The quick brown Fox jumps over the lazy dog.";
 // Console.WriteLine(pangram.ToLower().Contains("fox"));
