e8f6462e994018e2a8fcca224a3530e25fb37113
Paulo Cremas <cremascopaulo@gmail.com>
2023-11-03 02:54:24 -0300

    estudo-05

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,10 @@
 ﻿string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+
+// string pangram = "The quick brown Fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.ToLower().Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
 
 // Console.WriteLine(1 > 2);
 // Console.WriteLine(1 < 2);
