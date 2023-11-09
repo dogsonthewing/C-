5c80eab165c831b2c0fd54f9e485ed8d97fb46df
Paulo Cremas <cremascopaulo@gmail.com>
2023-11-09 00:47:58 -0300

    Method Lesson

--- PermissionNLevel/Program.cs
+++ PermissionNLevel/Program.cs
@@ -1,22 +1,2 @@
-﻿string permission = "Manager";
-int level = 13;
-
-if (permission.Contains("Admin"))
-{
-    if (level > 55)
-    {
-        Console.WriteLine("Welcome, Super Admin user.");
-    }
-    else
-    {
-        Console.WriteLine("Welcome, Admin user.");
-    }
-}
-else if (permission.Contains("Manager") && level >= 20)
-{
-    Console.WriteLine("Contact an Admin for access.");
-}
-else
-{
-    Console.WriteLine("You do not have sufficient privileges.");
-}
+﻿int result = 3 + 1 * 5 / 2;
+Console.WriteLine(result);
